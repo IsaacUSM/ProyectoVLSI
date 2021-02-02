4cf39946b7d3d7a14af1747ffa48c1d1fc1f4f5c
IsaacUSM <isaac0usm@gmail.com>
2021-02-02 13:46:18 -0600

    añado motor, teclado y divisor

--- MaquinaEXP.vhd
+++ MaquinaEXP.vhd
@@ -1,54 +1,306 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-ENTITY MaquinaEXP IS
-PORT(
-	clk, rst : IN STD_LOGIC; --Reloj del sistema
-	rw, rs, e : OUT STD_LOGIC; --read/write, setup/data, y enable para el lcd
-	lcd_data : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --Señales de datos para el lcd
-END MaquinaEXP;
-ARCHITECTURE behavior OF MaquinaEXP IS
-SIGNAL lcd_enable : STD_LOGIC;
-SIGNAL lcd_bus : STD_LOGIC_VECTOR(9 DOWNTO 0);
-SIGNAL lcd_busy : STD_LOGIC;
-COMPONENT lcd_controller IS
-PORT(
-	clk : IN STD_LOGIC;
-	reset_n : IN STD_LOGIC;
-	lcd_enable : IN STD_LOGIC;
-	lcd_bus : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-	busy : OUT STD_LOGIC;
-	rw, rs, e : OUT STD_LOGIC;
-	lcd_data : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
-END COMPONENT;
-BEGIN
-	--instancia del controlador de lcd
-	dut: lcd_controller
-	PORT MAP(clk => clk, reset_n => rst, lcd_enable => lcd_enable, lcd_bus => lcd_bus,
-	busy => lcd_busy, rw => rw, rs => rs, e => e, lcd_data => lcd_data);
-	PROCESS(clk)
-	VARIABLE char : INTEGER RANGE 0 TO 10 := 0;
+Library IEEE;
+Use IEEE.Std_logic_1164.all;
+Use IEEE.Std_logic_unsigned.all;
+entity MaquinaEXP is
+	Generic (FREQ_CLK: integer := 50_000_00);
+	Port( CLK: IN STD_LOGIC;
+			COLUMNAS: in std_logic_vector(3 downto 0);
+			FILAS: out std_logic_vector(3 downto 0);
+			BOTON_PRES: out std_logic_vector (3 downto 0);
+			IND: out std_logic;
+			PASS: OUT STD_LOGIC;
+			stop_motor: OUT STD_LOGIC:='0';
+			D0,D1,D2,D3,D4,D5,FANTASMA : BUFFER STD_LOGIC_VECTOR (6 DOWNTO 0));
+end MaquinaEXP;
+
+architecture behavioral of MaquinaEXP is
+	CONSTANT DELAY_1MS : INTEGER := (FREQ_CLK/1000)-1;
+	CONSTANT DELAY_10MS : INTEGER := (FREQ_CLK/100)-1;
+	SIGNAL CONTA_1MS: INTEGER RANGE 0 TO DELAY_1MS := 0;
+	SIGNAL BANDERA: STD_LOGIC := '0';
+	SIGNAL CONTA_10MS: INTEGER RANGE 0 TO DELAY_10MS := 0;
+	SIGNAL BANDERA2 : STD_LOGIC := '0';
+	SIGNAL BOT_1 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_2 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_3 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_4 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_5 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_6 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_7 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_8 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_9 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_A : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_B : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_C : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_D : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_0 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_AS : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL BOT_GA : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL FILA_REG_S : STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS=>'0');
+	SIGNAL FILA : INTEGER RANGE 0 TO 3 := 0;
+	SIGNAL IND_S : STD_LOGIC := '0';
+	SIGNAL EDO : INTEGER RANGE 0 TO 1 := 0;
+	SIGNAL FOCA : STD_LOGIC_VECTOR(4 DOWNTO 0) := "00000";
+	SIGNAL DESPLAZA : STD_LOGIC := '0';
 	BEGIN
-		IF(clk'EVENT AND clk = '1') THEN
-		IF(lcd_busy = '0' AND lcd_enable = '0') THEN
-		lcd_enable <= '1';
-		IF(char < 10) THEN
-		char := char + 1;
-		END IF;
-		CASE char IS
-		WHEN 1 => lcd_bus <= "1000110001";
-		WHEN 2 => lcd_bus <= "1000110010";
-		WHEN 3 => lcd_bus <= "1000110011";
-		WHEN 4 => lcd_bus <= "1000110100";
-		WHEN 5 => lcd_bus <= "1000110101";
-		WHEN 6 => lcd_bus <= "1000110110";
-		WHEN 7 => lcd_bus <= "1000110111";
-		WHEN 8 => lcd_bus <= "1000111000";
-		WHEN 9 => lcd_bus <= "1000111001";
-		WHEN OTHERS => lcd_enable <= '0';
-		END CASE;
-		ELSE
-		lcd_enable <= '0';
-		END IF;
-		END IF;
+		FILAS <= FILA_REG_S;
+		--RETARDO 1 MS--
+		PROCESS(CLK)
+		BEGIN
+			IF RISING_EDGE(CLK) THEN
+				CONTA_1MS <= CONTA_1MS+1;
+				BANDERA <= '0';
+				IF CONTA_1MS = DELAY_1MS THEN
+					CONTA_1MS <= 0;
+					BANDERA <= '1';
+				END IF;
+			END IF;
 		END PROCESS;
-END behavior;
+		----------------
+		--RETARDO 10 MS--
+		PROCESS(CLK)
+		BEGIN
+			IF RISING_EDGE(CLK) THEN
+				CONTA_10MS <= CONTA_10MS+1;
+				BANDERA2 <= '0';
+				IF CONTA_10MS = DELAY_10MS THEN
+					CONTA_10MS <= 0;
+					BANDERA2 <= '1';
+				END IF;
+			END IF;
+		END PROCESS;
+		----------------
+		--PROCESO EN LAS FILAS ----
+		PROCESS(CLK, BANDERA2)
+		BEGIN
+			IF RISING_EDGE(CLK) AND BANDERA2 = '1' THEN
+				FILA <= FILA+1;
+				IF FILA = 3 THEN
+					FILA <= 0;
+				END IF;
+			END IF;
+		END PROCESS;
+		WITH FILA SELECT
+			FILA_REG_S <= 	"1000" WHEN 0,
+								"0100" WHEN 1,
+								"0010" WHEN 2,
+								"0001" WHEN OTHERS;
+		-------------------------------
+		----------PROCESO EN EL TECLADO AL SELECCIONAR UN VALOR------------
+		PROCESS(CLK,BANDERA)
+		BEGIN
+			IF RISING_EDGE(CLK) AND BANDERA = '1' THEN
+				IF FILA_REG_S = "1000" THEN
+					BOT_1 <= BOT_1(6 DOWNTO 0)&COLUMNAS(3);
+					BOT_2 <= BOT_2(6 DOWNTO 0)&COLUMNAS(2);
+					BOT_3 <= BOT_3(6 DOWNTO 0)&COLUMNAS(1);
+					BOT_A <= BOT_A(6 DOWNTO 0)&COLUMNAS(0);
+				ELSIF FILA_REG_S = "0100" THEN
+					BOT_4 <= BOT_4(6 DOWNTO 0)&COLUMNAS(3);
+					BOT_5 <= BOT_5(6 DOWNTO 0)&COLUMNAS(2);
+					BOT_6 <= BOT_6(6 DOWNTO 0)&COLUMNAS(1);
+					BOT_B <= BOT_B(6 DOWNTO 0)&COLUMNAS(0);
+				ELSIF FILA_REG_S = "0010" THEN
+					BOT_7 <= BOT_7(6 DOWNTO 0)&COLUMNAS(3);
+					BOT_8 <= BOT_8(6 DOWNTO 0)&COLUMNAS(2);
+					BOT_9 <= BOT_9(6 DOWNTO 0)&COLUMNAS(1);
+					BOT_C <= BOT_C(6 DOWNTO 0)&COLUMNAS(0);
+				ELSIF FILA_REG_S = "0001" THEN
+					BOT_AS <= BOT_AS(6 DOWNTO 0)&COLUMNAS(3);
+					BOT_0 <= BOT_0(6 DOWNTO 0)&COLUMNAS(2);
+					BOT_GA <= BOT_GA(6 DOWNTO 0)&COLUMNAS(1);
+					BOT_D <= BOT_D(6 DOWNTO 0)&COLUMNAS(0);
+				END IF;
+			END IF;
+		END PROCESS;
+		----------------------------------------------------------------------------
+		--SALIDA--
+		PROCESS(CLK)
+		--variable DESPLAZA : integer range 0 to 6 := 0;
+		BEGIN
+			IF RISING_EDGE(CLK) THEN
+				IF BOT_0 = "11111111" THEN 
+					BOTON_PRES <= X"0"; 
+					FOCA <= "10000";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_1 = "11111111" THEN 
+					BOTON_PRES <= X"1"; 
+					FOCA <= "00001";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_2 = "11111111" THEN 
+					BOTON_PRES <= X"2"; 
+					FOCA <= "00010";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_3 = "11111111" THEN 
+					BOTON_PRES <= X"3"; 
+					FOCA <= "00011";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_4 = "11111111" THEN 
+					BOTON_PRES <= X"4"; 
+					FOCA <= "00100";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_5 = "11111111" THEN 
+					BOTON_PRES <= X"5"; 
+					FOCA <= "00101";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_6 = "11111111" THEN 
+					BOTON_PRES <= X"6"; 
+					FOCA <= "00110";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_7 = "11111111" THEN 
+					BOTON_PRES <= X"7"; 
+					FOCA <= "00111";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_8 = "11111111" THEN 
+					BOTON_PRES <= X"8"; 
+					FOCA <= "01000";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_9 = "11111111" THEN 
+					BOTON_PRES <= X"9"; 
+					FOCA <= "01001";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_A = "11111111" THEN 
+					BOTON_PRES <= X"A"; 
+					FOCA <= "01010";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_B = "11111111" THEN 
+					BOTON_PRES <= X"B"; 
+					FOCA <= "01011";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_C = "11111111" THEN 
+					BOTON_PRES <= X"C"; 
+					FOCA <= "01100";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_D = "11111111" THEN 
+					BOTON_PRES <= X"D"; 
+					FOCA <= "01101";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_AS = "11111111" THEN 
+					BOTON_PRES <= X"E"; 
+					FOCA <= "01110";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSIF BOT_GA = "11111111" THEN 
+					BOTON_PRES <= X"F"; 
+					FOCA <= "01111";
+					DESPLAZA <= '1';
+					IND_S <= '1';
+				ELSE 
+					IND_S <= '0';
+					DESPLAZA <= '0';
+				END IF;
+			END IF;
+		END PROCESS;
+		
+		WITH FOCA SELECT 
+		 FANTASMA<=  "1000000" WHEN "10000",-- 0  
+						 "1111001" WHEN "00001",-- 1
+						 "0100100" WHEN "00010",-- 2
+						 "0110000" WHEN "00011",-- 3
+						 "0011001" WHEN "00100",-- 4
+						 "0010010" WHEN "00101",-- 5
+						 "0000010" WHEN "00110",-- 6
+						 "0111000" WHEN "00111",-- 7
+						 "0000000" WHEN "01000",-- 8
+						 "0011000" WHEN "01001",-- 9
+						 "0001000" WHEN "01010",-- A
+						 "0000011" WHEN "01011",-- B
+						 "1000110" WHEN "01100",-- C
+						 "0100001" WHEN "01101",-- D
+						 "0011100" WHEN "01110",-- *
+						 "0001001" WHEN "01111",-- #
+						 "1111111" WHEN OTHERS;
+		-----------------------------		
+		
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D0 <= FANTASMA;
+			END IF;
+		END PROCESS;
+
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D1 <= D0;
+			END IF;
+		END PROCESS;	
+
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D2 <= D1;
+			END IF;
+		END PROCESS;
+
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D3 <= D2;
+			END IF;
+		END PROCESS;
+		
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D4 <= D3;
+			END IF;
+		END PROCESS;
+		
+		PROCESS(DESPLAZA)
+		BEGIN
+			IF DESPLAZA = '1' THEN
+				D5 <= D4;
+			END IF;
+		END PROCESS;
+
+		--- Se activa cuando se ingresa la pass
+		PROCESS(CLK)--159DA*
+		BEGIN
+			IF D5 = "1111001" AND D4 = "0010010" AND D3 = "0011000" AND D2 = "0100001" AND D1 = "0001000" AND D0 = "0011100" THEN
+				PASS <= '1';
+				stop_motor <= '1';
+			ELSE
+				PASS <= '0';
+				stop_motor <= '0';
+			END IF;
+		END PROCESS;
+		
+		--ACTIVACIÓN PARA LA BANDERA UN CICLO DE RELOJ--
+		PROCESS(CLK)
+		BEGIN
+			IF RISING_EDGE(CLK) THEN
+				IF EDO = 0 THEN
+					IF IND_S = '1' THEN
+						IND <= '1';
+						EDO <= 1;
+					ELSE
+						EDO <= 0;
+						IND <= '0';
+					END IF;
+				ELSE
+					IF IND_S = '1' THEN
+						EDO <= 1;
+						IND <= '0';
+					ELSE
+						EDO <= 0;
+					END IF;
+				END IF;
+			END IF;
+		END PROCESS;
+	--------------------------------------
+END behavioral;
---
+++ motor.vhd
@@ -0,0 +1,130 @@
+Library IEEE;
+Use IEEE.Std_logic_1164.all;
+Use IEEE.Std_logic_arith.all;
+Use IEEE.Std_logic_unsigned.all;
+Entity motor is
+	Port (reloj, reset, stop: in std_logic;
+			dato_motor: out std_logic_vector(3 downto 0));
+End motor;
+Architecture behavioral of motor is
+	Component divisor is
+	Port( reloj: in std_logic;
+			div_reloj : out std_logic);
+	end Component;
+	Type state is (inicia, cero, uno, dos, tres, cuatro, cinco, seis, siete);
+	Signal pr_state, nx_state : state;
+	Signal reloj_1 : std_logic;
+	SIGNAL cuenta : INTEGER RANGE 0 TO 4461 := 0;
+	SIGNAL vueltas : INTEGER RANGE 0 TO 4 := 0;
+begin
+	U1 : divisor Port map(reloj, reloj_1);
+	Process (reset, reloj_1)
+	Begin
+		If (reset='0') then
+			pr_state <= inicia;
+		elsif reloj_1 ='1' and reloj_1'event then
+			pr_state <= nx_state;
+		End if;
+	End Process;
+	
+	Process (reset, reloj_1, cuenta, vueltas)
+	begin
+		if reloj_1 ='1' and reloj_1'event then
+			cuenta <= cuenta + 1;
+			If cuenta = 4460 then
+				cuenta <= 0;
+				vueltas <= vueltas + 1;
+			else
+				If vueltas = 3 then
+					vueltas <= 0;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	Process(pr_state, stop, nx_state)
+	Begin
+		Case pr_state is
+		When inicia =>
+			If stop='0' then
+				nx_state <= inicia;
+			else
+				nx_state <= cero;
+			End If;
+		When cero =>
+			If stop='0' then
+				nx_state <= cero;
+			else
+				nx_state <= uno;
+			End If;
+		When uno =>
+			If stop='0' then
+				nx_state <= uno;
+			else
+				nx_state <= dos;
+			End If;
+		When dos =>
+			If stop='0' then
+				nx_state <= dos;
+			else
+				nx_state <= tres;
+			End If;
+		When tres =>
+			If stop='0' then
+				nx_state <= tres;
+			else
+				nx_state <= cuatro;
+			End If;
+		When cuatro =>
+			If stop='0' then
+				nx_state <= cuatro;
+			else
+				nx_state <= cinco;
+			End If;
+		When cinco =>
+			If stop='0' then
+				nx_state <= cinco;
+			else
+				nx_state <= seis;
+			End If;
+		When seis =>
+			If stop='0' then
+				nx_state <= seis;
+			else
+				nx_state <= siete;
+			End If;
+		When siete =>
+			If stop='0' then
+				nx_state <= siete;
+			else
+				nx_state <= inicia;
+			End If;
+		End Case;
+	End Process;
+	
+	Process(pr_state)
+	Begin
+		Case pr_state is
+		When inicia => 
+			dato_motor <= "0000";
+		When cero => 
+			dato_motor <= "1000";
+		When uno => 
+			dato_motor <= "1100";
+		When dos => 
+			dato_motor <= "0100";
+		When tres => 
+			dato_motor <= "0110";
+		When cuatro => 
+			dato_motor <= "0010";
+		When cinco => 
+			dato_motor <= "0011";
+		When seis => 
+			dato_motor <= "0001";
+		When siete => 
+			dato_motor <= "1001";
+		When others => 
+			NULL;
+		End Case;
+	End Process;
+End behavioral;
